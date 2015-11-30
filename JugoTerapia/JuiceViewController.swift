23830302092e4904e9f27ebc14bec92da1c76027
neodevelop <neodevelop@gmail.com>
2015-11-30 00:22:44 -0600

    Adding outlets and labels to the controller

---
+++ JugoTerapia/JuiceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  JuiceViewController.swift
+//  JugoTerapia
+//
+//  Created by José Juan Reyes Zuniga on 30/11/15.
+//  Copyright © 2015 MakingDevs. All rights reserved.
+//
+
+import UIKit
+
+class JuiceViewController: UIViewController {
+  
+  @IBOutlet weak var juiceNameLabel: UILabel!
+  @IBOutlet weak var juiceIngredientesLabel: UILabel!
+  @IBOutlet weak var juiceRecipeLabel: UILabel!
+}
