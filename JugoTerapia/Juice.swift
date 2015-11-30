4be276e05714bfd658b85844c653bf79494c2de8
neodevelop <neodevelop@gmail.com>
2015-11-29 23:22:34 -0600

    Adding the juices list

---
+++ JugoTerapia/Juice.swift
@@ -0,0 +1,19 @@
+//
+//  Juice.swift
+//  JugoTerapia
+//
+//  Created by José Juan Reyes Zuniga on 29/11/15.
+//  Copyright © 2015 MakingDevs. All rights reserved.
+//
+
+import Foundation
+
+struct Juicy {
+  let id: Int
+  let juiceName: String
+  
+  init(id:Int, juiceName: String){
+    self.id = id
+    self.juiceName = juiceName
+  }
+}
---
+++ JugoTerapia/JuicyListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  JuicyListViewController.swift
+//  JugoTerapia
+//
+//  Created by José Juan Reyes Zuniga on 29/11/15.
+//  Copyright © 2015 MakingDevs. All rights reserved.
+//
+
+import UIKit
+
+class JuicyListViewController: UITableViewController {
+  
+  var juices:[Juice] = []
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    <#code#>
+  }
+ 
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    <#code#>
+  }
+}
