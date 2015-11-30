0748d8989cd40e4bbbf9f4b3d196a83d2d686486
neodevelop <neodevelop@gmail.com>
2015-11-29 18:35:34 -0600

    Initial Commit

---
+++ JugoTerapia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JugoTerapia
+//
+//  Created by José Juan Reyes Zuniga on 29/11/15.
+//  Copyright © 2015 MakingDevs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
